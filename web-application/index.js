b13debfecbb1e5a566d556c3ab865abe836d7764
mrivasxvale <mrivas@crossvale.com>
2024-01-28 21:58:54 -0600

    Update index.js

--- web-application/index.js
+++ web-application/index.js
@@ -1,21 +1,14 @@
-// Configure event listener to get data
 getButton.addEventListener("click", () => {
-    //fetch('http://localhost:5140/weather')
-    fetch('https://front-end-service-default.apps-crc.testing/weather')
+    fetch('https://front-end-service-test-script.apps-crc.testing/weather')
         .then(response => { 
             return response.json()
         })
         .then(data => {
             
             var html  = '<table id=\'dataset\'><tr><th>DATE</th><th>SUMMARY</th><th>TEMPERATURE F</th><th>TEMPERATURE C</th>'
-            //document.getElementById('my-table')
-            //        .insertAdjacentHTML('beforeend', '<table id=\'dataset\'><tr><th>DATE</th><th>SUMMARY</th><th>TEMPERATURE F</th><th>TEMPERATURE C</th>')
-            
+
             data.forEach(forecast => {
                 html+= `<tr><td>${forecast.date}<td><td>${forecast.summary}<td><td>${forecast.temperatureF}<td><td>${forecast.temperatureC}<td></tr>`
-                
-                //document.getElementById('my-table')
-                //    .insertAdjacentHTML('beforeend', forecastHtml)
             })
 
             html+= '</table>'
